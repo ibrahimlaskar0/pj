cd2f6341c7cfa760e5cf4d1310ff5f1bd7cf276b
Ibrahim Laskar <ibrahimlaskar546@gmail.com>
2025-03-21 11:18:58 +0530

    jj

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+import { getAuth, createUserWithEmailAndPassword } from "./firebase-config.js";
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Rescue Bites Website Loaded!");
     
@@ -5,4 +6,26 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => {
         document.querySelector(".stat-box h2").innerText = "100";
     }, 3000);
+
+    
+    const auth = getAuth();
+    
+    document.getElementById('registerForm').addEventListener('submit', (e) => {
+      e.preventDefault();
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+    
+      // createUserWithEmailAndPassword(auth, email, password)
+      //   .then((userCredential) => {
+      //     console.log('User registered:', userCredential.user);
+      //     // Redirect to dashboard or login
+      //     console.log("succcesss")
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error:', error);
+      //   });
+    //   console.log("test")
+    alert("test")
+    });
+    
 });
